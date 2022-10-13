5531717824174e5143721c39f7e0479e54144fbd
austinsamuel <austinsamuel733@gmail.com>
2022-10-13 23:17:17 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.vehicle.vehicle();
+  enteredText = '';
+
+  onInput(value: string){
+    console.log(value);
+    this.enteredText = value;
+  }
 }
